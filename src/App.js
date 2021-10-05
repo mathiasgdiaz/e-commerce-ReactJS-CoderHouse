523d20c5b0265608e9e18c443a35342828f05d90
mathiasgdiaz <mathiasgdiaz@gmail.com>
2021-10-04 22:57:16 -0300

    Primer commit ReactJS Coder House
    Primer commit Proyecto e-commerce Curso ReactJS

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primer commit Proyecto Curso ReactJS 
+        </p>
+        <span><strong>Coder House</strong></span>
+        <br />
+        <a
+          className="App-link"
+          href="#"
+          rel="noopener noreferrer"
+        >
+          e-commerce coming soon!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
