1c009bbd947ac55f8691b07aa0d627c301d7fdb9
mathiasgdiaz <mathiasgdiaz@gmail.com>
2021-10-14 15:54:34 -0300

    Adding Tailwind to the project
    Adding Tailwind to project and add Tailwind CSS to a component for test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="ptest" class="p6 items-center justify-center">
           Primer commit Proyecto Curso ReactJS 
         </p>
         <span><strong>Coder House</strong></span>
