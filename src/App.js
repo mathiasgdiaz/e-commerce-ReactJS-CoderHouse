9a2a7d591f7a8b6c8abb712df2acff04a7ad3c91
mathiasgdiaz <mathiasgdiaz@gmail.com>
2021-10-14 19:05:38 -0300

    La Tiendita first commit
    - Added brand La Tiendita and nav with elements into the index page
    - Added component NavBar.jsx
    - Fixed problems with Taildwind
    - Added libraries headlessui and heroicons at the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/output.css'
+import NavBar from './components/NavBar.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="ptest" class="p6 items-center justify-center">
-          Primer commit Proyecto Curso ReactJS 
-        </p>
-        <span><strong>Coder House</strong></span>
-        <br />
-        <a
-          className="App-link"
-          href="#"
-          rel="noopener noreferrer"
-        >
-          e-commerce coming soon!
-        </a>
+  return(
+    <div>
+      <NavBar />
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-xl font-bold text-gray-900">Home</h1>
+        </div>
       </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* <!-- Replace with your content --> */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg p-40 h-96 flex justify-center">
+              <h2 className="text-xl font-bold text-gray-900">Próximamente las ofertas de la semana</h2>
+            </div>
+          </div>
+          {/* <!-- /End replace --> */}
+        </div>
+      </main>
     </div>
   );
 }
